3005c1196416a9c9bc4fc066e78ca508661341e1
Alon Peer <alon.peer@gmail.com>
2019-06-05 21:21:15 +0200

    Get a single song to play with hard-coded parameters

--- resources/lib/script.py
+++ resources/lib/script.py
@@ -5,17 +5,28 @@ from resources.lib import kodilogging
 import logging
 import xbmcaddon
 import xbmcgui
-
+import xbmc
+import requests
+import json
 
 ADDON = xbmcaddon.Addon()
 logger = logging.getLogger(ADDON.getAddonInfo('id'))
 
 
-# Put your code here, this is just an example showing
-# a textbox as soon as this addon gets called
-def show_dialog():
-    addon_name = ADDON.getAddonInfo('name')
+def run():
+    uuid = 'FOOBAR'
+    headers = {'Content-Type': 'application/json', 'X-UUID': uuid}
 
-    line1 = "Hello World!"
+    # Set artists
+    payload = {'ids': [66, 52, 43]}
+    artists_url = 'https://api.shiriapp.org.il/api/session/current-artist/set-all'
+    requests.post(artists_url, headers=headers, data=json.dumps(payload), verify=False)
 
-    xbmcgui.Dialog().ok(addon_name, line1)
+    # Get song
+    next_url = 'https://api.shiriapp.org.il/api/session/next-song'
+    song = requests.post(next_url, headers=headers, verify=False).json()
+    file = song['file_url']
+    title = song['title'].encode('utf8')
+    listitem = xbmcgui.ListItem(title)
+    listitem.setInfo('video', {'Title': title})
+    xbmc.Player().play(file, listitem)
