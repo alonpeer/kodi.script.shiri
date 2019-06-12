f51092138515ae621cebb9188cf8c90830f1d328
Alon Peer <alon.peer@gmail.com>
2019-06-12 14:53:08 +0200

    Create a playlist of 10 items and play them

--- resources/lib/script.py
+++ resources/lib/script.py
@@ -24,16 +24,7 @@ def get_uuid():
         kodiutils.set_setting('uuid', uuid)
     return uuid
 
-def run():
-    uuid = get_uuid()
-    headers = {'Content-Type': 'application/json', 'X-UUID': uuid}
-
-    # Set artists
-    payload = {'ids': [10, 11, 12, 13,  14]}
-    artists_url = 'https://api.shiriapp.org.il/api/session/current-artist/set-all'
-    requests.post(artists_url, headers=headers, data=json.dumps(payload), verify=False)
-
-    # Get song
+def next_song(headers):
     next_url = 'https://api.shiriapp.org.il/api/session/next-song'
     song = requests.post(next_url, headers=headers, verify=False).json()
     file = song['file_url']
@@ -56,4 +47,23 @@ def run():
         'mediatype': 'song',
         'duration': duration
     })
-    xbmc.Player().play(file, listitem)
+    listitem.setPath(file)
+    return listitem
+
+def run():
+    uuid = get_uuid()
+    headers = {'Content-Type': 'application/json', 'X-UUID': uuid}
+
+    # Set artists
+    payload = {'ids': [10, 11, 12, 13,  14]}
+    artists_url = 'https://api.shiriapp.org.il/api/session/current-artist/set-all'
+    requests.post(artists_url, headers=headers, data=json.dumps(payload), verify=False)
+
+    playlist = xbmc.PlayList(xbmc.PLAYLIST_MUSIC)
+    playlist.clear()
+
+    for i in range(10):
+        listitem = next_song(headers)
+        playlist.add(listitem.getPath(), listitem, i+1)
+
+    xbmc.Player().play(playlist)
